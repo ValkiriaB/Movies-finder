12acc955039ded494295823188a3f298e80eeea2
Valeria <valeriabarki@gmail.com>
2024-02-03 02:59:59 -0300

    change en main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import FavoritesProvider from "./context/FavoritesContext";
+import  {FavoritesProvider}   from "./context/FavoritesContext.jsx";
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
